a8905b05af5b616e9439bfd54f1b60f6ad003d56
MilaAnikieva <ludanikieva@gmail.ru>
2019-10-07 20:50:30 +0300

    Lesson 4 homework

---
+++ Les_4_hard.py
@@ -0,0 +1,10 @@
+# Задание-1:
+# Матрицы в питоне реализуются в виде вложенных списков:
+# Пример. Дано:
+matrix = [[1, 0, 8],
+          [3, 4, 1],
+          [0, 4, 2]]
+
+
+rotate_matrix = [x for x in list(map(list, zip(*matrix)))]
+print(rotate_matrix)
